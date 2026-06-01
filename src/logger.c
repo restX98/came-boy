5f1d192bda6e7d8dfa6ef5c914d5f5ea2bf42a39
Enrico Restuccia <enrico.rest98@gmail.com>
2026-06-01 22:43:26 +0100

    Add minimum log level filtering

--- src/logger.c
+++ src/logger.c
@@ -5,7 +5,12 @@
 
 static const char *level_strings[] = { "DEBUG", "INFO", "WARN", "ERROR" };
 
+static log_level_t min_level = LOG_DEBUG;
+
 void log_msg(log_level_t level, const char *file, int line, const char *fmt, ...) {
+    if (level < min_level) {
+        return;
+    }
     fprintf(stderr, "[%s]\t%s:%d: ", level_strings[level], file, line);
     va_list args;
     va_start(args, fmt);
